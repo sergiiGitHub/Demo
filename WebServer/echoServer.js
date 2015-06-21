14b33e6290fd70d65dffc296a29d8da523198cb6
Serge <m.sergiigithub@gmail.com>
2015-06-21 17:59:25 +0300

    [ ClientServer ] Practice with node.js #17            [ Type ] Practice with node.js           [ Issue ] Demo-17          [ Module ] ClientServer     [ Description ] add request operation

--- WebServer/echoServer.js
+++ WebServer/echoServer.js
@@ -1,6 +1,26 @@
 var http = require('http');
+var url = require('url');
+
 var server = new http.Server( function(req, res){
+    //GET /echo?m=Hello
     console.log(req.method, req.url);
+
+    // prse url & check string
+    var urlParser = url.parse( req.url, true );
+    // .....
+    // query: 'm=Hello',
+    // pathname: '/echo'
+    // .....
+    console.log( urlParser );
+
+    if ( urlParser.pathname == '/echo' && urlParser.query.message ){
+        res.end( urlParser.query.message );
+    }else {
+        res.statusCode = 404;
+        res.end( "Page not Found" );
+    }
+
+
 });
 
 server.listen(3000, '192.168.0.102');
