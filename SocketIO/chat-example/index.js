8d1967bec0c8cca227b3fc3d32157aebb2e8cc54
Serge <m.sergiigithub@gmail.com>
2015-06-23 22:09:44 +0300

    [ ClientServer ] Create СlientServer apk with help of socket.io #16 	   [ Type ] Task       [ Issue ] ClientServer-16      [ Module ] Demo [ Description ] Fix Socket.io chat massege

--- SocketIO/chat-example/index.js
+++ SocketIO/chat-example/index.js
@@ -4,13 +4,15 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  console.log( req.headers );
+  console.log(req.method, req.url);
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function(){
-	console.log('user disconnected');
-  })
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
