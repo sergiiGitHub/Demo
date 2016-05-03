07199fda7c924070f29399955a09bd008c77a861
Sergii <m.sergiigithub@gmail.com>
2016-05-03 12:19:08 +0300

    [ Android ] create request for Nearest place serveices        [ Type ] Task       [ Issue ] None      [ Module ] Android [ Description ] implement multi url image load, update.

---
+++ Android/NearbyInterest/app/src/main/java/com/example/sergii/nearbyinterest/LocationTracker.java
@@ -0,0 +1,78 @@
+package com.example.sergii.nearbyinterest;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.util.Pair;
+
+/**
+ * Created by sergii on 10.04.16.
+ */
+public class LocationTracker implements LocationListener {
+    private static final String TAG = LocationTracker.class.getSimpleName();
+
+    private boolean isLocationChange = true;
+    private Location location;
+
+    public void startLocationTracking( Context aContext ) {
+        if (ActivityCompat.checkSelfPermission(aContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(aContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "NOT start tracking ");
+            return;// TODO: 10.04.16 ask
+        }
+
+        LocationManager locationManager = (LocationManager) aContext.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Getting Current Location From GPS
+        Log.d(TAG, "start tracking");
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if(location!=null){
+            onLocationChanged(location);
+        }
+
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location aLocation) {
+        isLocationChange = true;
+        Log.d(TAG, "onLocationChanged: " + location.getLatitude() + ";  " + location.getLongitude());
+//        mLatitude = location.getLatitude();
+//        mLongitude = location.getLongitude();
+        location = aLocation;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public boolean isLocationChange() {
+        return isLocationChange && location != null;
+    }
+
+    public Location getCurrentLocation() {
+        isLocationChange = false;
+        return location;
+    }
+}
--- Android/NearbyInterest/app/src/main/java/com/example/sergii/nearbyinterest/MainActivity.java
+++ Android/NearbyInterest/app/src/main/java/com/example/sergii/nearbyinterest/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.sergii.nearbyinterest;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 
 import com.example.sergii.nearbyinterest.recycleview_provider.ItemModel;
@@ -31,7 +26,6 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends FragmentActivity implements
         View.OnClickListener,
-        LocationListener,
         RequestSender.ReceiveResultListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -40,26 +34,24 @@ public class MainActivity extends FragmentActivity implements
             .append("&key=AIzaSyAm5EsTdiPxwju2oYF5PMT7JJdM82gBcIc")
             .append("&photoreference=");
 
-    private final StringBuilder SB_REQUEST_PALCE = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
+    private final StringBuilder SB_REQUEST_PLACE = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
         .append("radius=5000")
         .append("&sensor=true")
         .append("&key=AIzaSyAm5EsTdiPxwju2oYF5PMT7JJdM82gBcIc");//server key request
+
     private static final int THREAD_NUM = 2;
-    private double mLatitude = -34;
-    private double mLongitude = 151;
+
     private RecyclerView recyclerView;
     private ItemModelAdapter modelAdapter;
     private ExecutorService executorService;
     private Handler handler;
-    private boolean isLocationChange;
+    private LocationTracker location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        startLocationTracking();
-
     }
 
     private void init() {
@@ -74,34 +66,13 @@ public class MainActivity extends FragmentActivity implements
 
         executorService = Executors.newFixedThreadPool(THREAD_NUM);
         handler = new Handler();
+        location = new LocationTracker();
+        location.startLocationTracking(this);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-    }
-
-    private void startLocationTracking() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "NOT start tracking ");
-            return;
-        }
-
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Getting Current Location From GPS
-        Log.d(TAG, "start tracking");
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        if(location!=null){
-            onLocationChanged(location);
-        }
-
-        locationManager.requestLocationUpdates(provider, 20000, 0, this);
     }
 
     @Override
@@ -116,45 +87,27 @@ public class MainActivity extends FragmentActivity implements
 
     private String getRequest() {
 
-        if ( !isLocationChange ){
+        if ( !location.isLocationChange() ){
             return null;
         }
-        Log.d(TAG, "onClick: location=" + mLatitude + "," + mLongitude);
-        return new StringBuilder(SB_REQUEST_PALCE.toString())
+        //    private double mLatitude = -34;
+//    private double mLongitude = 151;
+        Location loc = location.getCurrentLocation();
+        Log.d(TAG, "onClick: location=" + loc.getLatitude() + "," + loc.getLongitude());
+        return new StringBuilder(SB_REQUEST_PLACE.toString())
                 .append("&types=food")
                 .append("&location=")
-                .append(mLatitude)
+                .append(loc.getLatitude())
                 .append(",")
-                .append(mLongitude)
+                .append(loc.getLongitude())
                 .toString();
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        isLocationChange = true;
-        Log.d(TAG, "onLocationChanged: " + location.getLatitude() + ";  " + location.getLongitude());
-        mLatitude = location.getLatitude();
-        mLongitude = location.getLongitude();
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
+    //Parser
     @Override
     public void onReceiveResult(String aResult) {
         //// TODO: 07.04.16 in work thread
+        // TODO write unit test
         // check time
         Log.d(TAG, "onReceiveResult: " + aResult);
         JSONObject resultObject;
@@ -183,7 +136,7 @@ public class MainActivity extends FragmentActivity implements
 
                     Log.d(TAG, "onReceiveResult: name: " + name + "; width: " + width);
                     item.setReference(ref);
-                    requestBitmap(item);
+                    doInWorkThread(new BitmapLoader(item));
                 }
                 modelAdapter.appndItem(item);
             }
@@ -197,12 +150,12 @@ public class MainActivity extends FragmentActivity implements
         String getReference();
     }
 
-    private void doInMainThread( Runnable run ){
+    private void doInMainThread( final Runnable run ){
         handler.post(run);
     }
 
-    private void requestBitmap(final BitmapListener aListener) {
-        executorService.execute(new BitmapLoader(aListener));
+    private void doInWorkThread(final Runnable run ) {
+        executorService.execute(run);
     }
 
     private String getType(JSONObject itemObject) throws JSONException {
