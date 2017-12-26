6a695086cdaee8d8be37bae2d859508ce45ae2ff
Sergii <m.sergiigithub@gmail.com>
2017-12-26 22:18:50 +0200

    Improve Angular: lessons 2         [ Type ] web | front-end         [ Language ] Angular         [ Description ] create todo list

---
+++ Angular/lessons2/todo.js
@@ -0,0 +1,33 @@
+angular.module('todoApp', [])
+.controller('TodoListController', function() {
+var todoList = this;
+todoList.todos = [
+  {text:'learn AngularJS', done:true},
+  {text:'build an AngularJS app', done:false}];
+
+todoList.addTodo = function() {
+// check for not add empty field
+  if({text:todoList.todoText} && {text:todoList.todoText.length} != 0 && todoList.todoText != '') {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
+  }
+};
+
+todoList.remaining = function() {
+  var count = 0;
+  angular.forEach(todoList.todos, function(todo) {
+    count += todo.done ? 0 : 1;
+  });
+  return count;
+};
+
+todoList.archive = function() {
+  var oldTodos = todoList.todos;
+  todoList.todos = [];
+  angular.forEach(oldTodos, function(todo) {
+    if (!todo.done) todoList.todos.push(todo);
+  });
+};
+});
+
+
