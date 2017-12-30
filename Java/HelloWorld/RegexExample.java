864559f6c3069c04dedfce9ed06e039d7b2ddd2f
Sergii <m.sergiigithub@gmail.com>
2017-12-30 19:37:35 +0200

    Improve Regex add read by group     [ Type ] Regex     [ Language ] Java     [ Description ] add read by group

--- Java/HelloWorld/RegexExample.java
+++ Java/HelloWorld/RegexExample.java
@@ -1,6 +1,7 @@
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class RegexExample{
+public class RegexExample {
 
 	public static void main(String[] args) {
 
@@ -10,43 +11,57 @@ public class RegexExample{
 		Matcher m = p.matcher("abaaaba");
 		// the source
 		System.out.println("ab");
-		while(m.find()) {
-			System.out.print( m.start() + " ");
+		while (m.find()) {
+			System.out.print(m.start() + " ");
 		}
-		
+
 		// part 2
 		System.out.println();
 		p = Pattern.compile("0[xX][0-9a-fA-F]");
 		m = p.matcher("12 0x 0x12 0Xf 0xg");
 		System.out.println("0[xX][0-9a-fA-F]:");
-		while(m.find()) {
-			System.out.print( m.start() + " ");
-		}	
-		
+		while (m.find()) {
+			System.out.print(m.start() + " ");
+		}
+
 		System.out.println();
-		p = Pattern.compile("12[^3]"); // except 3 ^, 
+		p = Pattern.compile("12[^3]"); // except 3 ^,
 		m = p.matcher("123124123");
 		System.out.println("12[^3]");
-		while(m.find()) {
+		while (m.find()) {
 			System.out.print(m.start() + " ");
 		}
-		
+
 		System.out.println();
-		p = Pattern.compile(".*xx"); // except 3 ^, 
+		p = Pattern.compile(".*xx"); // except 3 ^,
 		m = p.matcher("yyxxxyxx");
 		System.out.println(".*xx");
-		while(m.find()) {
+		while (m.find()) {
 			System.out.print(m.start() + " ");
 		}
-		
-		
+
 		System.out.println();
-		p = Pattern.compile(".*?xx"); // except 3 ^, 
+		p = Pattern.compile(".*?xx"); // except 3 ^,
 		m = p.matcher("yyxxxyxx");
 		System.out.println(".*?xx");
-		while(m.find()) {
+		while (m.find()) {
 			System.out.print(m.start() + " ");
+		}
+
+		// advanced regex
+		advancedRegex("2003/2/12 text (inside text)");
+	}
+
+	private static void advancedRegex(String str) {
+		System.out.println();
+		final Pattern p = Pattern
+				.compile("(\\d+/\\d+/\\d+)\\s(\\w+)\\s\\((.*?)\\)");
+		// the expression
+		System.out.println(str);
+		final Matcher m = p.matcher(str);
+		System.out.println("is: " + m.find() + "; groups: " + m.groupCount());
+		for (int i = 1; i <= m.groupCount(); ++i) {
+			System.out.println("> " + i + ". " + m.group(i));
 		}
 	}
 }
-
